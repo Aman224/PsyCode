e106b6869ec98ff4c2749fa63c8ea04c50642a5f
Aman O <amanclct@gmail.com>
2020-02-24 13:16:02 +0530

    Adds dropdown functionality

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 from .models import Content
 
+LANGUAGES = (
+    ('c','C'),
+    ('python','Python'),
+)
+
 class InputForm(forms.ModelForm):
+
+    inputData = forms.CharField(widget=forms.Textarea, label='')
 
     class Meta:
         model = Content
-        fields = ('inputData', )
-        attrs = {'class': 'form-contr'}
+        fields = ('language', 'inputData')
+        
+    def __init__(self, *args, **kwargs):
+        super(InputForm, self).__init__(*args, **kwargs)
+        self.fields['inputData'].widget.attrs['placeholder'] = 'Enter Pseudocode'
--- webapp/models.py
+++ webapp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
+
+LANGUAGES = (
+    ('c','C'),
+    ('python','Python'),
+)
+
+
 class Content(models.Model):
     inputData = models.TextField()
+    language = models.CharField(max_length=10, choices=LANGUAGES)
     outputData = models.TextField()
 
     def get_absolute_url(self):
